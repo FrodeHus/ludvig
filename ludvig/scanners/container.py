40dba0095f340b171b2508811bfaa3d7c82c37b3
Frode Hus <frode.hus@easee.com>
2023-01-17 08:44:18 +0100

    refactored sample extraction

--- ludvig/scanners/container.py
+++ ludvig/scanners/container.py
@@ -60,26 +60,27 @@ class ImageScanner:
             matches = self.yara.match(data=data.read())
             for match in matches:
                 orig_offset = match.strings[0][0]
-                prefix_offset = 10 if orig_offset > 10 else orig_offset
-                offset = orig_offset - prefix_offset
-                data.seek(offset)
-                try:
-                    snippet = data.read(len(match.strings[0][2]) + prefix_offset).decode(
-                        "utf-8"
-                    )
-                except UnicodeDecodeError as e:
-                    data.seek(orig_offset)
-                    snippet = data.read(len(match.strings[0][2])).decode("utf-8")
-                    # snippet = ''.join(format(x, '02x') for x in snippet)
-                yield SecretFinding(YaraRuleMatch(snippet, match), file.name, layer)
+                sample = self.__read_secret_sample(data, orig_offset, len(match.strings[0][2]))
+                yield SecretFinding(YaraRuleMatch(sample, match), file.name, layer)
         except Exception as ex:
             return print(ex)
         finally:
             data.close()
             
-    def __read_secret_sample(self, content : bytes) -> str:
-        content.fi
-        
+    def __read_secret_sample(self, data : bytes, offset : int, match_len : int) -> str:
+        orig_offset = offset
+        prefix_offset = 10 if orig_offset > 10 else orig_offset
+        offset = orig_offset - prefix_offset
+        data.seek(offset)
+        try:
+            sample = data.read(match_len + prefix_offset).decode(
+                "utf-8"
+            )
+        except UnicodeDecodeError as e:
+            data.seek(orig_offset)
+            sample = data.read(match_len).decode("utf-8")
+            # sample = ''.join(format(x, '02x') for x in sample)
+        return sample
 
     def __decode_content(self, content: str) -> str:
         for match in self.__possible_base64_encoding(content):
