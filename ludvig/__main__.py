768b12e29f622efcb1e868ce4d35eda0696b80e6
Frode Hus <frode.hus@outlook.com>
2023-01-21 00:29:55 +0100

    moving all output to own module (#23)

--- ludvig/__main__.py
+++ ludvig/__main__.py
@@ -1,17 +1,15 @@
 import json
-import pprint
 import sys
 import tarfile
 import argparse
 from typing import List
 from ludvig.client import DockerClient
+from ludvig.outputs.pretty import PrettyConsole
 from ludvig.rules.loader import load_yara_rules
 from ludvig.types import Finding, Image, Layer, Severity
 from ludvig.scanners.filesystem import FilesystemScanner
 from ludvig.scanners.container import ImageScanner
-from rich.table import Table
 from rich.progress import Progress
-from rich.console import Console
 import yara
 
 
@@ -24,6 +22,9 @@ def main():
     )
     parser.add_argument(
         "--custom-rules", help="Path to custom YARA rules (need to have .yar extension)"
+    )
+    parser.add_argument(
+        "--output", help="Output format", choices=["pretty"], default="pretty"
     )
     parser.add_argument(
         "--level",
@@ -48,65 +49,14 @@ def main():
         progress.remove_task(scan_task)
 
     if len(findings) > 0:
-        output(findings, args.deobfuscated)
+        output_provider = get_output_provider(args.output, findings, args.deobfuscated)
+        output_provider.output()
         sys.exit(2)
 
-
-def output(findings: List[Finding], deobfuscated: bool = False):
-    table = Table(title="Findings", show_lines=True)
-    table.add_column("Rule", style="white")
-    table.add_column("Filename", style="white", overflow="fold")
-    table.add_column("Content", style="red")
-    for finding in findings:
-        table.add_row(
-            "{}: {}\r\n[gray50]{}[/]".format(
-                color_coded_severity(finding.match.severity),
-                finding.match.rule_name,
-                ", ".join(finding.match.tags),
-            ),
-            "{} {}\r\n[gray50]{}{}[/]".format(
-                finding.filename,
-                (":cross_mark:" if finding.whiteout else ""),
-                "Created by: [yellow]{}[/]".format(prettify(finding.comment)),
-                "\r\nRemoved by: [yellow]{}[/]".format(prettify(finding.removed_by))
-                if finding.removed_by
-                else "",
-            ),
-            format_samples(finding, deobfuscated),
-        )
-
-    console = Console()
-    console.print(table)
-
-
-def format_samples(finding: Finding, deobfuscated=False):
-    output = ""
-    for sample in finding.samples:
-        output += "[yellow]{0:<5d}[/]: {1}\r\n".format(
-            sample.offset, sample.content if deobfuscated else sample.obfuscated_content
-        )
-    return output
-
-
-def prettify(s: str) -> str:
-    if s is None:
-        return s
-    s = s[: s.index("#") if "#" in s else len(s)]
-    return s.replace("/bin/sh -c", "")
-
-
-def color_coded_severity(severity: Severity):
-    match severity:
-        case "MEDIUM":
-            return "[yellow]{0:<10s}[/]".format(severity)
-        case "HIGH":
-            return "[magenta]{0:<10s}[/]".format(severity)
-        case "CRITICAL":
-            return "[red]{0:<10s}[/]".format(severity)
-        case _:
-            return "[bright_black]{0:<10s}[/]".format(severity)
-
-
+def get_output_provider(provider : str, findings : List[Finding], deobuscated = False):
+    if provider == "pretty":
+        return PrettyConsole(findings, deobuscated)
+    
 def scan_image(
     image: str, rules: yara.Rules, severity_level: Severity = Severity.MEDIUM
 ) -> List[Finding]:
