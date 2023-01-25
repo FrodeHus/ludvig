3e87ea8aec1d102a28de271cfaee28e9cdf03cc5
Frode Hus <frode.hus@outlook.com>
2023-01-25 08:59:59 +0100

    added workaround for knack to return exit code if positive scan results are present (#38)

---
+++ ludvig/_help.py
@@ -0,0 +1,11 @@
+from knack.help_files import helps
+
+helps['image'] = """
+type: group
+short-summary: Operations related to container images
+"""
+
+helps['fs'] = """
+type: group
+short-summary: Operations related to file system
+"""
