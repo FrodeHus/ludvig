197857ef12868896f332b3215e740069751593b1
Frode Hus <frode.hus@outlook.com>
2023-01-26 10:06:59 +0100

    Support ludvigignore file (#41)
    * support for .ludvignore
    
    * updated docs

--- ludvig/providers/_providers.py
+++ ludvig/providers/_providers.py
@@ -1,6 +1,22 @@
+from typing import List
+import os
+import fnmatch
+
+
 class BaseFileProvider:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, exclusions: List[str] = None) -> None:
+        self.exclusions = exclusions or []
+        self.__read_ignore_file()
 
     def get_files(self):
         pass
+
+    def is_excluded(self, filename: str) -> bool:
+        for exclude in self.exclusions:
+            return fnmatch.fnmatch(filename, exclude)
+
+    def __read_ignore_file(self):
+        if os.path.exists(".ludvignore"):
+            with open(".ludvignore", "r") as fp:
+                ignore_data = fp.readlines()
+                self.exclusions.extend(ignore_data)
