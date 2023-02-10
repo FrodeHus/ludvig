f2f62ddf31abaa6d0fadfe75e2ddaa6da4498b21
frodehus <frode.hus@outlook.com>
2023-02-10 17:30:56 +0100

    added validations

--- ludvig/vulndb/_osv.py
+++ ludvig/vulndb/_osv.py
@@ -1,6 +1,7 @@
 # https://ossf.github.io/osv-schema/
 
 
+import re
 from typing import List
 
 
@@ -18,24 +19,43 @@ class OSVReference:
 
 class OSVEvent:
     def __init__(self, **kwargs) -> None:
+        self.__allowed = ["introduced", "fixed", "last_affected", "limit"]
         """
         Defines a package event.
         Can only one of either introduced, fixed, last_affected or limit.
         """
         [(name, value)] = list(kwargs.items())
-        assert name in ["introduced", "fixed", "last_affected", "limit"]
+        assert name in self.__allowed
         setattr(self, name, value)
+        self.__validate()
+
+    def __validate(self):
+        for a in self.__allowed:
+            if hasattr(self, a):
+                value = getattr(self, a)
+                if not validate_rfc3339_date(value):
+                    raise Exception("'{}' does not match RFC3339 date format".format(a))
 
 
 class OSVRange:
-    def __init__(
-        self, type: str, repo: str = None, events: List[OSVEvent] = []
-    ) -> None:
+    def __init__(self, type: str, events: List[OSVEvent], repo: str = None) -> None:
         self.type: str = type
         self.repo: str = repo
         self.events: List[OSVEvent] = events
         self.database_specific: str = None
         self.ecosystem_specific: dict = {}
+        self.__validate()
+
+    def __validate(self):
+        if self.type == "git" and not self.repo:
+            raise Exception("repo is required when type is 'git'")
+        if (
+            not self.events
+            or not len([e for e in self.events if hasattr(e, "introduced")]) >= 1
+        ):
+            raise Exception(
+                "events must have atleast one occurence of an 'introduced' event"
+            )
 
 
 class OSVPackage:
@@ -43,6 +63,29 @@ class OSVPackage:
         self.ecosystem: str = ecosystem
         self.name: str = name
         self.package_url: str = package_url
+        self.__validate()
+
+    def __validate(self):
+        valid_ecosystems = [
+            "go",
+            "npm",
+            "oss-fuzz",
+            "pypi",
+            "rubygems",
+            "crates.io",
+            "packagist",
+            "maven",
+            "nugest",
+            "linux",
+            "debian",
+            "alpine",
+            "hex",
+            "android",
+            "githubactions",
+            "pub",
+        ]
+        if self.ecosystem not in valid_ecosystems:
+            raise Exception("{} is not a valid ecosystem".format(self.name))
 
 
 class OSVAffected:
@@ -78,3 +121,35 @@ class OSVulnerability:
         self.affected: List[OSVAffected] = affected
         self.references: List[OSVReference] = references
         self.database_specific: dict = database_specific
+        self.__validate()
+
+    def __validate(self):
+        valid_id_prefixes = [
+            "A-",
+            "GO-",
+            "OSV-",
+            "PYSEC-",
+            "RUSTSEC-",
+            "GSD-",
+            "GHSA-",
+            "LBSEC-",
+            "DSA-",
+            "DLA-",
+            "DTSA-",
+            "CVE-",
+        ]
+        if not self.id.startswith(tuple(valid_id_prefixes)):
+            raise Exception("not a valid id - {}".format(self.id))
+
+        if not validate_rfc3339_date(self.modified):
+            raise Exception("'modified' does not match RFC3339 date format")
+
+
+def validate_rfc3339_date(date_str: str):
+    rfc3339 = r"^((?:(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2}(?:\.\d+)?))(Z|[\+-]\d{2}:\d{2})?)$"
+    if re.match(
+        rfc3339,
+        date_str,
+    ):
+        return True
+    return False
--- tests/test_vulndb_ingesters.py
+++ tests/test_vulndb_ingesters.py
@@ -1,6 +1,7 @@
 import os
 from unittest import TestCase
 from ludvig.vulndb.ingesters import github_read_advisory, github_read_repository
+from ludvig.vulndb import OSVPackage, OSVRange, OSVEvent, OSVulnerability
 
 
 class TestParseGitHubAdvisory(TestCase):
@@ -19,3 +20,70 @@ class TestParseGitHubAdvisory(TestCase):
         repo = os.path.join(os.path.dirname(__file__), "fixtures/ghsa")
         advisories = list(github_read_repository(repo))
         self.assertEqual(len(advisories), 2)
+
+
+class TestOpenSourceVulnerabilityValidations(TestCase):
+    def test_id_prefix_validation(self):
+        expected_prefixes = [
+            "A",
+            "GO",
+            "OSV",
+            "PYSEC",
+            "RUSTSEC",
+            "GSD",
+            "GHSA",
+            "LBSEC",
+            "DSA",
+            "DLA",
+            "DTSA",
+            "CVE",
+        ]
+        for prefix in expected_prefixes:
+            id = "%s-test-advisory" % prefix
+            with self.subTest(prefix=prefix):
+                OSVulnerability(id, "2023-01-01T15:21:32.59+02:00", [], [])
+
+    def test_ecosystem_validation(self):
+        expected_ecosystems = [
+            "go",
+            "npm",
+            "oss-fuzz",
+            "pypi",
+            "rubygems",
+            "crates.io",
+            "packagist",
+            "maven",
+            "nugest",
+            "linux",
+            "debian",
+            "alpine",
+            "hex",
+            "android",
+            "githubactions",
+            "pub",
+        ]
+        for ecosystem in expected_ecosystems:
+            with self.subTest(ecosystem=ecosystem):
+                OSVPackage(ecosystem, "test")
+
+    def test_range_validation(self):
+        test_cases = [
+            {
+                "type": "git",
+                "repo": "test/repo",
+                "events": [OSVEvent(introduced="2023-01-01T15:21:32.59+02:00")],
+            },
+            {
+                "type": "semver",
+                "repo": None,
+                "events": [OSVEvent(introduced="2023-01-01T15:21:32.59+02:00")],
+            },
+            {
+                "type": "ecosystem",
+                "repo": None,
+                "events": [OSVEvent(introduced="2023-01-01T15:21:32.59+02:00")],
+            },
+        ]
+        for case in test_cases:
+            with self.subTest(type=case["type"], repo=case["repo"]):
+                OSVRange(case["type"], case["events"], repo=case["repo"])
